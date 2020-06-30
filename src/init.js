e1b28a700f85506d13c3d0c719a0a6222ab69e65
Bekki Young <moonbladez@hotmail.com>
2020-06-30 13:19:46 +0200

    eslint prettier setup

---
+++ src/init.js
@@ -0,0 +1,25 @@
+const TICK_RATE = 3000;
+
+const tick = () => {
+	console.log("tick", Date.now());
+};
+
+const init = () => {
+	console.log("starting game");
+
+	let nextTimeToTick = Date.now();
+
+	const nextAnimationFrame = () => {
+		const now = Date.now();
+
+		if (nextTimeToTick <= now) {
+			tick();
+			nextTimeToTick = now + TICK_RATE;
+		}
+		requestAnimationFrame(nextAnimationFrame);
+	};
+
+	requestAnimationFrame(nextAnimationFrame);
+};
+
+init();
